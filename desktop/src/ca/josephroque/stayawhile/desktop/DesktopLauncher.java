886d63a3691530ddc26b871dfb86f00e90d99d71
Joseph Roque <contact@josephroque.ca>
2016-06-24 19:49:47 -0400

    Initial libGDX project commit

---
+++ desktop/src/ca/josephroque/stayawhile/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ca.josephroque.stayawhile.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ca.josephroque.stayawhile.StayAwhile;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StayAwhile(), config);
+	}
+}
